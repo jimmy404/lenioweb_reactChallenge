57d34571791092cb7536600bfd8433acfd636fbf
Jimmy200 <bruno_pier@hotmail.com>
2021-06-01 23:54:39 -0300

    Adds wrapper for context in _app file

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import '../styles/globals.css';
+import { AppWrapper } from '../context/AppContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
 
 MyApp.propTypes = {
