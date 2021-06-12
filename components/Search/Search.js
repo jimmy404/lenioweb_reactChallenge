b7669eead6e563616a2fea3561e3caabe2527932
Jimmy200 <bruno_pier@hotmail.com>
2021-06-12 02:10:20 -0300

    Improves styles in Search

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,30 +1,20 @@
 import React from 'react';
 
+import { useAppContext } from '../../context/AppContext';
+
 import styled from 'styled-components';
+import styles from './stylesSearch';
 
 import Star from '../Star/Star';
 import Brand from '../Brand/Brand';
 import SearchInput from '../SearchInput/SearchInput';
-import { useAppContext } from '../../context/AppContext';
 
 const NavBar = styled.nav`
-  align-items: center;
-  background: #fff;
-  box-shadow: rgb(0 0 0) 0px 3px 15px -8px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  max-height: 80px;
-  min-height: 80px;
-  padding: 20px;
-  position: fixed;
-  top: 0;
-  width: 100%;
+  ${styles.NavBar}
 `;
 
 const StarContainer = styled.div`
-  height: 30px;
-  width: 30px;
+  ${styles.StarContainer}
 `;
 
 const Search = () => {
