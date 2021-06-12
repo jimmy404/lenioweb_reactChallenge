2f2522dc615de6e274cc6bc36a267135ae534345
Jimmy200 <bruno_pier@hotmail.com>
2021-06-12 02:12:42 -0300

    Improves styles in comic page

---
+++ styles/comicStyles.js
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+const styles = {
+  ComicPageContainer: css`
+    height: calc(100vh - 80px);
+    margin-top: 80px;
+    padding: 60px;
+    width: 100vw;
+  `
+};
+
+export default styles;
