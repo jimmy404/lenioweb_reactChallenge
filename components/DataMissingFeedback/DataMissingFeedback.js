fb852d59576316f9d432b51b6b6a8a751e8a6e67
Jimmy200 <bruno_pier@hotmail.com>
2021-06-12 02:06:21 -0300

    Improves styles in DataMissingFeedback

--- components/DataMissingFeedback/DataMissingFeedback.js
+++ components/DataMissingFeedback/DataMissingFeedback.js
@@ -1,30 +1,15 @@
 import React from 'react';
-
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
+import styles from './stylesDataMissingFeedback';
+
 const DataMissingFeedbackContainer = styled.div`
-  align-items: center;
-  display: flex;
-  height: 100%;
-  justify-content: center;
-  width: 100%;
+  ${styles.DataMissingFeedbackContainer}
 `;
 
 const DataMissingFeedbackContent = styled.div`
-  background-color: white;
-  border-radius: 4px;
-  color: #f0141e;
-  display: flex;
-  flex-direction: column;
-  font-size: 24px;
-  font-weight: bold;
-  height: 100%;
-  justify-content: center;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-  width: 100%;
+  ${styles.DataMissingFeedbackContent}
 `;
 
 const DataMissingFeedback = ({ message }) => {
---
+++ components/DataMissingFeedback/stylesDataMissingFeedback.js
@@ -0,0 +1,28 @@
+import { css } from 'styled-components';
+
+const styles = {
+  DataMissingFeedbackContainer: css`
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    width: 100%;
+  `,
+  DataMissingFeedbackContent: css`
+    background-color: white;
+    border-radius: 4px;
+    color: #f0141e;
+    display: flex;
+    flex-direction: column;
+    font-size: 24px;
+    font-weight: bold;
+    height: 100%;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    width: 100%;
+  `
+};
+
+export default styles;
