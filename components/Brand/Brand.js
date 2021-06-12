b7a023c8775ada454b5210b6be9e8944e4c9c618
Jimmy200 <bruno_pier@hotmail.com>
2021-06-12 02:03:37 -0300

    Improves styles in Brand

--- components/Brand/Brand.js
+++ components/Brand/Brand.js
@@ -3,15 +3,10 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 import styled from 'styled-components';
+import styles from './stylesBrand';
 
 const BrandContainer = styled.a`
-  background-image: url('https://logodownload.org/wp-content/uploads/2017/05/marvel-logo-4.png');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  display: inline-block;
-  height: 40px;
-  width: 115px;
+  ${styles.BrandContainer}
 `;
 
 const Brand = () => {
---
+++ components/Brand/stylesBrand.js
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+
+const styles = {
+  BrandContainer: css`
+    background-image: url('https://logodownload.org/wp-content/uploads/2017/05/marvel-logo-4.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    display: inline-block;
+    height: 40px;
+    width: 115px;
+  `
+};
+
+export default styles;
